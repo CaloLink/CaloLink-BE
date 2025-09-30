f7974449d9560359b05f96c485cb00d7420e8d0c
KING <dbswjddn6@gmail.com>
2025-09-30 12:58:32 +0900

    change FoodDetailResponse

--- calolink/src/main/java/shop/calolink/domain/dto/FoodDetailResponse.java
+++ calolink/src/main/java/shop/calolink/domain/dto/FoodDetailResponse.java
@@ -2,15 +2,13 @@ package shop.calolink.domain.dto;
 
 import shop.calolink.Category;
 
+import java.util.List;
+
 public record FoodDetailResponse(
         Long id,
         String name,
         String manufacturer,
         Category category,
-        Integer kcal,
-        Integer carbs,
-        Integer protein,
-        Integer fat,
-        String imageUrl
-) {
-}
+        List<NutritionResponse> nutritionResponse
+) {}
+
