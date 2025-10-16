8cac3cff9b480ba6ac13472a2eaeebd00be474ef
KING <dbswjddn6@gmail.com>
2025-10-16 16:31:26 +0900

    FEAT: 음식 검색 및 상세 조회 서비스 로직 구현

--- calolink/src/main/java/shop/calolink/service/FoodService.java
+++ calolink/src/main/java/shop/calolink/service/FoodService.java
@@ -1,13 +1,71 @@
 package shop.calolink.service;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import shop.calolink.Category;
+import shop.calolink.domain.dto.FoodDetailResponse;
+import shop.calolink.domain.dto.FoodSummaryResponse;
+import shop.calolink.domain.entity.Food;
+import shop.calolink.repository.FoodRepository;
 
-
-import java.util.List;
-
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Service
 public class FoodService {
+
+    private final FoodRepository foodRepository;
+
+    public Page<FoodSummaryResponse> searchFoods(String name, Category category, Pageable pageable) {
+        Page<Food> foodPage;
+        // ❗️ 빈 문자열도 체크하도록 수정
+        boolean hasName = (name != null && !name.isEmpty());
+        boolean hasCategory = (category != null);
+
+        // ❗️❗️ 모든 경우의 수를 처리하도록 if-else 구조를 반드시 수정해야 합니다.
+        if (hasName && hasCategory) {
+            foodPage = foodRepository.findByNameContainingAndCategory(name, category, pageable);
+        } else if (hasName) {
+            foodPage = foodRepository.findByNameContaining(name, pageable);
+        } else if (hasCategory) {
+            foodPage = foodRepository.findByCategory(category, pageable);
+        } else {
+            // ❗️ 아무 조건도 없을 때 (ex: 앱 첫 진입) 전체 목록을 반환
+            foodPage = foodRepository.findAll(pageable);
+        }
+
+        // FoodSummaryResponse::from == food -> FoodSummaryResponse.from(food)
+        return foodPage.map(FoodSummaryResponse::from);
+    }
+
+    public FoodDetailResponse getFoodDetailById(Long id) {
+        Food food = foodRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 음식을 찾을 수 없습니다."));
+        return FoodDetailResponse.from(food);
+    }
+
+
+
+//    @Transactional
+//    public void createFoodFromDto(FoodSummaryResponse summaryResponse) {
+//
+//        int roundedCarbs = (int) Math.round(summaryResponse.carbs());
+//        int roundedProtein = (int) Math.round(summaryResponse.protein());
+//        int roundedFat = (int) Math.round(summaryResponse.fat());
+//
+//        Nutrition nutrition = Nutrition.builder()
+//                .carb(roundedCarbs)
+//                .protein(roundedProtein)
+//                .fat(roundedFat)
+//                .build();
+//
+//        Food food = Food.builder()
+//                .name(summaryResponse.name())
+//                .imageUrl(summaryResponse.imageUrl())
+//                .nutrition(nutrition)
+//                .build();
+//
+//        foodRepository.save(food);
+//    }
 
 }
