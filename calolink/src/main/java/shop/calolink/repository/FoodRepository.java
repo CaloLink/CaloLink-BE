3ea6fb866769dd865d3d881dc6d2558b91ebe2e3
KING <dbswjddn6@gmail.com>
2025-10-16 16:27:57 +0900

    FEAT: 음식 검색을 위한 메서드 추가

--- calolink/src/main/java/shop/calolink/repository/FoodRepository.java
+++ calolink/src/main/java/shop/calolink/repository/FoodRepository.java
@@ -1,8 +1,16 @@
 package shop.calolink.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import shop.calolink.Category;
 import shop.calolink.domain.entity.Food;
 
 public interface FoodRepository extends JpaRepository<Food, Long> {
 
+    Page<Food> findByNameContaining(String name, Pageable pageable);
+
+    Page<Food> findByCategory(Category category, Pageable pageable);
+
+    Page<Food> findByNameContainingAndCategory(String name, Category category, Pageable pageable);
 }
