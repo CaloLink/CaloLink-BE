bb1588e550a0e298996f5347afc8ba4bce8b7b67
KING <dbswjddn6@gmail.com>
2025-10-16 16:33:22 +0900

    FEAT: 음식 API 앤드포인트 구현

--- calolink/src/main/java/shop/calolink/controller/FoodController.java
+++ calolink/src/main/java/shop/calolink/controller/FoodController.java
@@ -1,13 +1,44 @@
 package shop.calolink.controller;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import shop.calolink.domain.entity.Food;
+import shop.calolink.Category;
+import shop.calolink.domain.dto.FoodDetailResponse;
+import shop.calolink.domain.dto.FoodSummaryResponse;
+import shop.calolink.service.FoodService;
 
-import java.util.List;
-
+@RequiredArgsConstructor
 @RestController
-@RequestMapping("/foods")
+@RequestMapping("/api/foods")
 public class FoodController {
 
+    private final FoodService foodService;
 
+    /**
+     * 음식 목록 검색 및 조회 API
+     * 예시 URL: /api/foods?category=NOODLES&page=0&size=10
+     */
+    @GetMapping
+    public ResponseEntity<Page<FoodSummaryResponse>> getFoodSummary(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) Category category,
+            Pageable pageable) {
+        Page<FoodSummaryResponse> foods = foodService.searchFoods(name, category, pageable);
+
+        return ResponseEntity.ok(foods);
+    }
+
+    /**
+     * 음식 상세 정보 조회 API
+     * 예시 URL: /api/foods/1
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<FoodDetailResponse> getFoodDetail(@PathVariable Long id) {
+        FoodDetailResponse foodDetail = foodService.getFoodDetailById(id);
+        return ResponseEntity.ok(foodDetail);
+    }
 }
